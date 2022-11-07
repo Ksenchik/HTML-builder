c2d2a97aaf0bef2e73d654a2753744997648f096
Ksenchik <oksana.100@mail.ru>
2022-11-07 22:13:45 +0300

    fix: task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 const output = fs.createWriteStream(path.join(__dirname, "text.txt"), 'utf-8');
+const message = {
+   START: "enter text:\n",
+   FINISH: "bye!"
+ }
 
 const { stdin, stdout } = process;
+stdout.write("Введите текст: \n");
 
 stdin.on('data', data => {
     const dataStringified = data.toString();
     output.write('');
     output.write(dataStringified);
 });
+
+
 
